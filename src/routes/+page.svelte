f4fd922260e29de72e16efb04a5677b422b540c7
TangHoang <businesshoangtang@gmail.com>
2024-03-19 17:13:37 +0100

    added math questions page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,11 @@
 <script>
     import { emoji } from "./emoji.js"
+    import { questions} from "./mathQuestions.js"
 
     const State = {
         start: "start",
-        playing: "playing",
+        playingMemory: "playingMemory",
+        playingMath: "playingMath",
         paused: "paused",
         won: "won",
         lost: "lost",
@@ -12,16 +14,31 @@
     let state = State.start;
     let size = 20;
     let grid = createGrid();
+    let chosenQuestions = chooseQuestions();
+
+    let value = null;
+    let submittedAnswer = null;
+    let booleanArray = [] // used to keep track of correct or false answers
+
     let maxMatches = grid.length / 2;
     let selected = [];
     let matches = [];
     let timerId = null;
-    let time = 20;
+    let time = 120;
+    let elapsedTime = 0;
+
+    function chooseQuestions() {
+        let temp = []; 
+        for(let i=1; i<4; i++) {
+            temp.push(questions[Math.floor(Math.random() * questions.length)]);
+        }
+        return temp;
+    }
 
     function startGameTimer() {
         function countdown() {
             state !== State.paused && (time -= 1)
-            console.log(time);
+            elapsedTime += 1;
         }
 
         timerId = setInterval(countdown, 1000);
@@ -74,27 +91,48 @@
         selected = [];
         matches = [];
         timerId = null;
-        time = 20;
+        time = 120;
     }
 
-    $: if (state === State.playing) {
+    function playMath() {
+        state = State.playingMath;
+    }
+
+    function playMemory() {
+        chosenQuestions = chooseQuestions();
+        booleanArray = [];
+        state = State.playingMemory;
+    }
+
+    function handleInput(id, correctAnswer) {
+        let answer = document.getElementById(`${id}`);
+        if (answer.value == correctAnswer) {
+            booleanArray[id] = true;
+        }
+        return answer.value;
+    }
+
+    $: if (state === State.playingMemory) {
         if(timerId === null) startGameTimer();
     }
 
     $: selected.length === 2 && matchCards();
     $: maxMatches === matches.length && gameWon();
     $: time === 0 && gameLost();
+    $: booleanArray.length === 3 && playMemory();
+
+    $: elapsedTime % 30 === 2 && playMath()
 
     $: console.log(state, selected, matches);
 
 </script>
 
-{#if state === "start"}
+{#if state === State.start}
     <h1>Memory</h1>
-    <button on:click={() => state = State.playing}>Play</button>
+    <button on:click={() => state = State.playingMemory}>Play</button>
 {/if}
 
-{#if state === "playing"}
+{#if state === State.playingMemory}
     <h1 class="timer" class:pulse={time < 6}> {time}</h1>
     <div class="matches">
         {#each matches as card}
@@ -121,12 +159,27 @@
 
 {#if state === State.lost}
     <h1>Verloren!</h1>
-    <button on:click={() => state = State.playing}>Play Again</button>
+    <button on:click={() => state = State.playingMemory}>Play Again</button>
 {/if}
 
 {#if state === State.won}
     <h1>Gewonnen!</h1>
-    <button on:click={() => state = State.playing}>Play Again</button>
+    <button on:click={() => state = State.playingMemory}>Play Again</button>
+{/if}
+
+{#if state === State.playingMath}
+    <h1 class="timer" class:pulse={time < 6}> {time}</h1>
+    <div class="questions">
+        {#each chosenQuestions as question, questionId}
+            <form on:submit|preventDefault={() => { submittedAnswer = handleInput(questionId, question.answer)}} class="question-block">
+                <div class="question"> {question.question}</div>
+                <input class="answer" id="{questionId}" type="numeric">
+                {#if booleanArray[questionId] == true}
+                    <p>Correct</p>
+                {/if}
+            </form>
+        {/each}
+    </div>
 {/if}
 
 <style>
@@ -134,6 +187,39 @@
         display: grid;
         grid-template-columns: repeat(5, 1fr);
         gap: 0.5rem
+    }
+
+    .question {
+        height: 150px;
+        width: 500px;
+        background: var(--bg-2);
+        display: grid;
+        align-items: center;
+        padding-left: 30px;
+        padding-right: 30px;
+        border-radius: 5px;
+    }
+
+    .answer {
+        font-size: 4.5rem;
+        max-width: 20%;
+        text-align: center;
+        border: none;
+        border-radius: 5px;
+        background-color: whitesmoke;
+    }
+
+    .question-block {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        width: 800px;
+        width: fit-content;
+        font-size: 4.5rem;
+        margin-top: 10px;
+        margin-bottom: 10px;
+        border-radius: 8px;
+
     }
 
     .card {
