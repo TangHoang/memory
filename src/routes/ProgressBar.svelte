7bbfedbd7b1a8ca9c9d2b4ec73355036b373fdb2
TangHoang <businesshoangtang@gmail.com>
2024-03-20 12:19:20 +0100

    added Progressbar component

---
+++ src/routes/ProgressBar.svelte
@@ -0,0 +1,24 @@
+<script>
+    export let value = 0; // Input value for the progress bar
+</script>
+  
+<style>
+    .progress-bar-container {
+        width: 100%;
+        background-color: #f0f0f0;
+        border-radius: 8px;
+        margin: 30px 0;
+    }
+
+    .progress-bar {
+        height: 20px;
+        background-color: #007bff;
+        border-radius: 8px;
+        transition: width 0.3s ease;
+
+    }
+</style>
+
+<div class="progress-bar-container">
+    <div class="progress-bar" style="width: {value}%;"></div>
+</div>
